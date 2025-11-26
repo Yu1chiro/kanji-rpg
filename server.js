09a113a6dc0bf950b9e76d9b3072f054b586c917
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-11-27 04:15:36 +0800

    push

--- server.js
+++ server.js
@@ -30,19 +30,20 @@ app.get('/api/kanji', (req, res) => {
         }
     });
 });
-app.get('/api/boss-kanji', (req, res) => {
-    const filePath = path.join(__dirname, 'public', 'boss_kanji.json');
+
+app.get('/api/boss-quiz', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'boss-quiz.json');
     
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-            console.error("Error Boss DB:", err);
-            return res.status(500).json({ error: "Gagal memanggil Boss Data" });
+            console.error("Error Boss Quiz:", err);
+            return res.status(500).json({ error: "Gagal memanggil Quiz Boss" });
         }
         try {
             const jsonData = JSON.parse(data);
             res.json({ success: true, data: jsonData });
         } catch (parseErr) {
-            res.status(500).json({ error: "Data Boss Rusak" });
+            res.status(500).json({ error: "Data Quiz Rusak" });
         }
     });
 });
@@ -55,13 +56,33 @@ app.get('/api/status', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/info', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'info.html'));
+});
+app.get('/slash', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'audio', 'slash.mp3'));
+});
+app.get('/victory', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'audio', 'victory.mp3'));
+});
+app.get('/crack', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'audio', 'crack.mp3'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.get('/quiz', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'quiz.html'));
 });
 app.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'statistik.html'));
 });
-
+app.get('/boss-battle', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'boss-battle.html')); // ROUTE BARU KHUSUS BOSS
+});
+app.get('/preparation', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'preparation-battle.html'));
+});
 app.listen(PORT, () => {
     console.log(`Server Kanji RPG berjalan di http://localhost:${PORT}`);
 });
